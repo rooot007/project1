8c61c9fb30602680c95c45616aee26d4ed101709
Jun Tao <Jun@yahoo.com>
2018-03-06 23:19:23 +0200

    new changes from 6 mach

---
+++ lesson3.1.py
@@ -0,0 +1,12 @@
+import os
+from collections import OrderedDict
+#from requests import get, post
+
+d = OrderedDict([
+    ('pen', 3),
+    ('pinapple' , 2),
+    ('mango', 4),
+])
+print(d.keys())
+loop = str("location")
+print(loop)
